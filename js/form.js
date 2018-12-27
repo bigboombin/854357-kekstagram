a17d6bfb81ab3326d6e9835d8859e4fc401cdeaa
bigboombin <42147846+bigboombin@users.noreply.github.com>
2018-12-27 19:27:02 +0400

    форма

--- js/form.js
+++ js/form.js
@@ -6,6 +6,10 @@
   var hashtagsInput = document.querySelector('.text__hashtags');
   var comments = document.querySelector('.text__description');
   hashtagsInput.addEventListener('change', function () {
+    hashtagsInput.setCustomValidity('');
+    hashtagsInput.style = '';
+    comments.setCustomValidity('');
+    comments.style = '';
     var hashtagsInputStr = hashtagsInput.value;
     var newHashtags = hashtagsInputStr.toLowerCase();
     hashtagsInput.value = newHashtags;
@@ -14,30 +18,37 @@
     var hashtagLetters;
     if (hashtags.length > MAX_HASHTAGS) {
       hashtagsInput.setCustomValidity('нельзя указать больше пяти хэш-тегов');
+      hashtagsInput.style = 'border-color: red; outline-color: red;';
     }
     for (var i = 0; i < hashtags.length; i++) {
       hashtag = hashtags[i];
       hashtagLetters = hashtag.split('');
       if (hashtagLetters[0] !== '#' && hashtagLetters.length !== 0) {
         hashtagsInput.setCustomValidity('хэш-тег начинается с символа # (решётка)');
+        hashtagsInput.style = 'border-color: red; outline-color: red;';
       } else if (hashtagLetters.length > MAX_HASHTAG) {
         hashtagsInput.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку');
+        hashtagsInput.style = 'border-color: red; outline-color: red;';
       } else if (hashtagLetters.length === 1) {
         hashtagsInput.setCustomValidity('хеш-тег не может состоять только из одной решётки');
+        hashtagsInput.style = 'border-color: red; outline-color: red;';
       } else if (hashtagLetters[hashtagLetters.length - 1] === '#') {
         hashtagsInput.setCustomValidity('хэш-теги разделяются пробелами');
+        hashtagsInput.style = 'border-color: red; outline-color: red;';
       }
       for (var j = 0; j < hashtags.length; j++) {
         if (hashtag === hashtags[i + 1]) {
           hashtagsInput.setCustomValidity('один и тот же хэш-тег не может быть использован дважды');
+          hashtagsInput.style = 'border-color: red; outline-color: red;';
         }
       }
     }
   });
   comments.addEventListener('change', function () {
-    var commentsLetter = comments.value.split(' ');
+    var commentsLetter = comments.value;
     if (commentsLetter.length > MAX_COMMENT) {
       comments.setCustomValidity('длина комментария не может составлять больше 140 символов');
+      comments.style = 'border-color: red; outline-color: red;';
     }
   });
 })();
